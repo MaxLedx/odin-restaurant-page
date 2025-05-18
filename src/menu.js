41513dab562da68e5128c471c21425c16277e8e1
MaxLedx <80272454+MaxLedx@users.noreply.github.com>
2025-05-18 11:41:58 +0200

    feat: add basic content and styling

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,10 @@ export default function (root, document) {
     const container = document.createElement('div');
     const title = document.createElement('h1');
     title.textContent = "Menu";
+    const descripition = document.createElement('p');
+    descripition.textContent = 'This is where the menu would go.';
 
     container.appendChild(title);
+    container.appendChild(descripition);
     root.appendChild(container);
 }
